d50d03585233cadbc6f238be9e8bca0b8cd782c9
Nathan Guerrero <naguerr2@uci.edu>
2021-11-20 14:23:57 -0800

    More basic additions to results page

--- zothacks-2021/src/results/Results.js
+++ zothacks-2021/src/results/Results.js
@@ -1,28 +1,50 @@
+import './Results.css';
+
 const Results = () => {
+    // Currently placeholders, to be replaced when API implemented
+    let mainScore = -1;
+    let songScore1 = -1;
+    let songScore2 = -1;
+
+    let albumArt1 = "tempalbum1.jpg";
+    let albumArt2 = "tempalbum2.jpg";
+    let altAlbumArt1 = "Temporary placeholder 1";
+    let altAlbumArt2 = "Temporary placeholder 2";
+
+    let artistName1 = "Artist 1";
+    let artistName2 = "Artist 2";
+    let songName1 = "Song 1";
+    let songName2 = "Song 2";
+
+    const startAgain = () => {
+        
+    }
+
+    // JSX object
     return (
         <nav class="results">
             <div className="resultsTitle">
-                <h1>You got <strong>?/10.</strong> Congratulations!</h1>
-                <button>PLAY AGAIN</button>
+                <h1>You got <strong>{ mainScore }/10.</strong> Congratulations!</h1>
+                <button onClick={ startAgain }>PLAY AGAIN</button>
             </div>
             <div className="resultsInfoRow">
                 <div className="resultsInfoColumn">
                     <p>
-                        You got ?/2 on ... genre<br />
+                        You got { songScore1 }/2 on ... genre<br />
                         You are really a master of it!<br />
                         Here is a song we are recommending for you:
                     </p>
-                    <img src=""></img>
-                    <p>Info</p>
+                    <img src={ albumArt1 } alt={ altAlbumArt1 } class="resultsAlbumCover"></img>
+                    <p>{ artistName1 } - { songName1 }</p>
                 </div>
                 <div className="resultsInfoColumn">
                     <p>
-                        You got ?/2 on ... genre<br />
+                        You got { songScore2 }/2 on ... genre<br />
                         You are really a master of it!<br />
                         Here is a song we are recommending for you:
                     </p>
-                    <img src=""></img>
-                    <p>Info</p>
+                    <img src={ albumArt2 } alt={ altAlbumArt2 } className="resultsAlbumCover"></img>
+                    <p>{ artistName2 } - { songName2 }</p>
                 </div>
             </div>
         </nav>
