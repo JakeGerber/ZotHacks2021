c0364400114c1fe84264d8cc85dbb3c9650c944c
Nathan Guerrero <naguerr2@uci.edu>
2021-11-20 14:44:14 -0800

    Extra changes for results page

--- zothacks-2021/src/results/Results.js
+++ zothacks-2021/src/results/Results.js
@@ -6,6 +6,9 @@ const Results = () => {
     let songScore1 = -1;
     let songScore2 = -1;
 
+    let genre1 = "Stone"
+    let genre2 = "Words"
+
     let albumArt1 = "tempalbum1.jpg";
     let albumArt2 = "tempalbum2.jpg";
     let altAlbumArt1 = "Temporary placeholder 1";
@@ -30,18 +33,18 @@ const Results = () => {
             <div className="resultsInfoRow">
                 <div className="resultsInfoColumn">
                     <p>
-                        You got { songScore1 }/2 on ... genre<br />
-                        You are really a master of it!<br />
-                        Here is a song we are recommending for you:
+                        You got { songScore1 }/2 for { genre1 }<br />
+                        You've definitely mastered it!<br />
+                        Here's a song that we recommend for you:
                     </p>
                     <img src={ albumArt1 } alt={ altAlbumArt1 } class="resultsAlbumCover"></img>
                     <p>{ artistName1 } - { songName1 }</p>
                 </div>
                 <div className="resultsInfoColumn">
                     <p>
-                        You got { songScore2 }/2 on ... genre<br />
-                        You are really a master of it!<br />
-                        Here is a song we are recommending for you:
+                        You got { songScore2 }/2 for { genre2 }<br />
+                        Want to learn more about it?<br />
+                        Here's a song that we recommend for you:
                     </p>
                     <img src={ albumArt2 } alt={ altAlbumArt2 } className="resultsAlbumCover"></img>
                     <p>{ artistName2 } - { songName2 }</p>
